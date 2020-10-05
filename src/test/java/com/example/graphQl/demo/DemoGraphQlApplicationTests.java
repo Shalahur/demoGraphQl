9ab86ea6fffe8131357a1c4573e239acbe383653
Md.Shalahur Rahman <srpolash1991@gmail.com>
2020-10-05 15:42:48 +0600

    1st push
    1st push

---
+++ src/test/java/com/example/graphQl/demo/DemoGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphQl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
